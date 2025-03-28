40beb58f1cb6756bd8d15c9fc165ef0d94c3cb5f
K1000o-lp <camiloalfonsolopezparra@gmail.com>
2025-03-28 00:23:27 -0400

    feat: add quotes configuration file with default settings

---
+++ packages/camilo/quotes/src/config/quotes.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Quotes Configuration
+    |--------------------------------------------------------------------------
+    |
+    | This file stores basic data required for the quotes package to work correctly,  
+    | such as 'base_url', 'request_per_time', and 'duration_time'.
+    |
+    */
+
+    'base_url' => env('QUOTES_BASE_URL', 'https://dummyjson.com/quotes'),
+    'request_per_time' => env('QUOTES_REQUEST_PER_TIME', 10),
+    'duration_time' => env('QUOTES_DURATION_TIME', 60),
+];
