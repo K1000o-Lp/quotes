25516dea9970588d2d6520630b5bba8049e7d303
K1000o-lp <camiloalfonsolopezparra@gmail.com>
2025-03-28 03:23:49 -0400

    feat: organize quote routes and add random and search views

--- packages/camilo/quotes/src/routes/web.php
+++ packages/camilo/quotes/src/routes/web.php
@@ -10,8 +10,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('quotes-ui')->group(function () {
     Route::prefix('v1')->name('quotes-ui.v1.')->group(function () {
-        Route::get('/', function () {
-            return view('quotes::index');
-        })->name('index');
+        Route::prefix('quotes')->name('quotes.')->group(function () {
+            Route::get('/', function () {
+                return view('quotes::index');
+            })->name('index');
+    
+            Route::get('/random', function () {
+                return view('quotes::random');
+            })->name('random');
+    
+            Route::get('/search', function () {
+                return view('quotes::search');
+            })->name('search');
+        });
     });
+});
+
+Route::get('/', function () {
+    return redirect()->route('quotes-ui.v1.quotes.index');
 });
