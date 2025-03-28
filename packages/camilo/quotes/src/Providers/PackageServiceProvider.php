0a530c06fb0f2685092278ccffa26c783dccd857
K1000o-lp <camiloalfonsolopezparra@gmail.com>
2025-03-28 01:25:58 -0400

    feat: add HTTP macro for quotes with rate limiting functionality

--- packages/camilo/quotes/src/Providers/PackageServiceProvider.php
+++ packages/camilo/quotes/src/Providers/PackageServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Camilo\Quotes\Providers;
 
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
 
 class PackageServiceProvider extends ServiceProvider
@@ -14,6 +16,39 @@ class PackageServiceProvider extends ServiceProvider
 
     public function boot()
     {
+        // HTTP Macros
+        Http::macro('quotes', function () {
+            return Http::baseUrl(config('quotes.base_url'))->beforeSending(function ($request) {
+                $rateLimitKey = 'quotes_api_rate_limit';
+                $maxRequests = config('quotes.request_per_time');
+                $timeWindow = config('quotes.duration_time');
+
+                // Obtain the number of requests made
+                $requestCount = Cache::get($rateLimitKey, 0);
+
+                if ($requestCount >= $maxRequests) {
+                    // if the limit is reached, wait until the time window is over
+                    $resetTime = Cache::get($rateLimitKey . '_reset', now()->addSeconds($timeWindow));
+                    $waitTime = $resetTime->diffInSeconds(now());
+
+                    if ($waitTime > 0) {
+                        sleep($waitTime); // pause the execution
+                    }
+
+                    // reboot the counter
+                    Cache::put($rateLimitKey, 0, $timeWindow);
+                }
+
+                // increment the counter
+                Cache::increment($rateLimitKey);
+
+                // Set the reset time
+                if (!Cache::has($rateLimitKey . '_reset')) {
+                    Cache::put($rateLimitKey . '_reset', now()->addSeconds($timeWindow), $timeWindow);
+                }
+            });
+        });
+
         // Publish configurations
         $this->publishes([
             __DIR__ . '/../config/quotes.php' => config_path('quotes.php'),
