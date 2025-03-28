a329e3e71bd389da958ceab926bb229e1161c36c
K1000o-lp <camiloalfonsolopezparra@gmail.com>
2025-03-28 03:22:52 -0400

    feat: add Vue support and include quotes package in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,26 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css', 
+                'resources/js/app.js',
+                'packages/camilo/quotes/src/resources/js/app.js',
+                'packages/camilo/quotes/src/resources/css/app.css',
+            ],
             refresh: true,
         }),
         tailwindcss(),
+        vue(),
     ],
+    resolve: {
+        alias: {
+            '@quotes': '/packages/camilo/quotes/src/resources',
+            'vue': 'vue/dist/vue.esm-bundler.js',
+        },
+    },
 });
